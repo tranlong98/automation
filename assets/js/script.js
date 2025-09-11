91a34b7d44de32d0a729d053932668e645dbfccd
tranlong98 <103907601+tranlong98@users.noreply.github.com>
2025-09-11 23:58:37 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,39 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+/*music*/
+
+document.addEventListener('DOMContentLoaded', function() {
+    const audio = document.getElementById('background-music');
+
+    // Mảng chứa các đường dẫn URL trực tiếp đến file nhạc online
+    const musicList = [
+        'https://youtu.be/p-R0_4bohPk',
+        'https://youtu.be/stLrhYcR2VE',
+        'https://youtu.be/MsyiNSbeuEI'
+        // Thêm các URL trực tiếp khác vào đây
+    ];
+
+    // Chọn ngẫu nhiên một bài hát từ danh sách
+    const randomSong = musicList[Math.floor(Math.random() * musicList.length)];
+    audio.src = randomSong;
+
+    // Tự động phát nhạc sau khi người dùng tương tác với trang
+    document.body.addEventListener('click', function() {
+        audio.play().catch(e => {
+            console.error("Autoplay was prevented:", e);
+        });
+    }, { once: true });
+});
+
+
+
+
+
+
+
+
+
+
+
+
